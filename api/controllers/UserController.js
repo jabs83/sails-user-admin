b934e1991db275fc870fea4e5c93a3a340b336af
Jason <jasonb@dzap.net>
2014-03-30 23:19:40 -0700

    added edit, update, and index actions to the user controller.

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -53,6 +53,36 @@ module.exports = {
   	});
   },
 
+  index: function(req, res, next) {
+  	User.find(function(err, users){
+  		if(err) return next(err);
+  		res.view({
+  			users:users
+  		});
+  	});
+  },
+
+  //render the edit view
+  edit: function(req, res, next) {
+  	User.findOne(req.param('id'), function foundUser (err, user) {
+  		if(err) return next(err);
+  		if(!user) return next();
+  		res.view({
+  			user:user
+  		});
+  	});
+  },
+
+  update: function(req, res, next) {
+  	User.update(req.param('id'), req.params.all(), function userUpdated (err){
+  		if(err) {
+  			return res.redirect('user/edit' + req.param('id'));
+  		}
+
+  		res.redirect('user/show' + req.param('id'));
+  	});
+  },
+
   /**
    * Overrides for the settings in `config/controllers.js`
    * (specific to UserController)
